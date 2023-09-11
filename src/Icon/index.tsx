b52d17843ad5e6004e831d4a022c059f65f87dd6
Jake Adams <jake.adams@blackbridge.co.uk>
2023-09-11 13:45:18 +0100

    + Icon

---
+++ src/Icon/index.tsx
@@ -0,0 +1,25 @@
+import './style.css'
+import classnames from 'classnames'
+
+
+
+
+// export default function () {
+
+// 	const icon = <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="60" height="60" fill="#D9D9D9"/></svg>	
+
+// 	return icon
+// }
+
+// const test 
+
+// export function test() {
+
+// }
+
+
+
+export const Twitter = () => ( 
+	<svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="60" height="60" fill="#D9D9D9"/></svg>
+)
+

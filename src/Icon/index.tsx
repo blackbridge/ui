e88eb6abff9501d6be132bf64fc1039c34f894bc
Jake Adams <jake.adams@blackbridge.co.uk>
2023-09-12 14:12:06 +0100

    Icon: add external icon

--- src/Icon/index.tsx
+++ src/Icon/index.tsx
@@ -2,24 +2,13 @@ import './style.css'
 import classnames from 'classnames'
 
 
-
-
-// export default function () {
-
-// 	const icon = <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="60" height="60" fill="#D9D9D9"/></svg>	
-
-// 	return icon
-// }
-
-// const test 
-
-// export function test() {
-
-// }
-
-
-
 export const Twitter = () => ( 
 	<svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="60" height="60" fill="#D9D9D9"/></svg>
 )
 
+export function External() {
+	return <svg width="650" height="650" viewBox="0 0 650 650" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M191.667 625H605C616.047 625 625 616.047 625 605V45C625 33.9543 616.047 25 605 25H45C33.9543 25 25 33.9543 25 45V458.333" stroke="black" stroke-width="50" stroke-linecap="round" stroke-linejoin="round"/>
+	<path d="M41.6667 608.333L325 325M325 325V458.333M325 325H191.667" stroke="black" stroke-width="50" stroke-linecap="round" stroke-linejoin="round"/>
+	</svg>	
+}

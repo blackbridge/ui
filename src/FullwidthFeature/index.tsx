7df2a497feb5d8ff87e5300b0cdfb335441dfd6b
Jake Adams <jake.adams@blackbridge.co.uk>
2023-10-30 14:53:22 +0000

    Fullwidth feature

---
+++ src/FullwidthFeature/index.tsx
@@ -0,0 +1,45 @@
+import './style.css'
+import '../utility.css'
+
+import type { JSX, ComponentChildren } from "preact"
+import classnames from 'classnames'
+import Container from '../Container/index.tsx'
+
+type FullwidthFeatureProps = {
+	class?: HTMLElement['className'],
+	style?: string,
+	image?: string,
+	overlay?: boolean,
+	children?: ComponentChildren
+}
+
+export default function FullwidthFeature(props: FullwidthFeatureProps): JSX.Element {
+
+	const {
+		class: className, 
+		children,
+		image,
+		overlay = false,
+		...attributes
+	} = props
+
+	const classes = classnames(
+		'ui-fullwidth-feature grid flex-center',
+		className,
+	)
+
+	const attrs = { class: classes, ...attributes }
+
+	return <>
+			<div class={classes}>
+				<div class="ui-fullwidth-feature__content">
+					<Container>
+						{children}
+					</Container>
+				</div>
+				{overlay && <div class="ui-fullwidth-feature__overlay object-fit--absolute"></div>}
+				<img class="ui-fullwidth-feature__media object-fit--absolute" src={image} />
+			</div>
+		</>
+		
+}

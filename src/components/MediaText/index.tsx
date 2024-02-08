8f77440f7d2365d0d3ca6565abcd5d12feceac88
Jake Adams <jake.adams@blackbridge.co.uk>
2024-02-08 16:27:39 +0000

    MediaText: Add breakpoints

--- src/components/MediaText/index.tsx
+++ src/components/MediaText/index.tsx
@@ -1,6 +1,6 @@
-import './style.css'
-import '../../global/grid.less'
-import '../../utility.css'
+import './style.less'
+import '../../styles/grid.less'
+import '../..//styles/utility.less'
 
 import type { JSX, ComponentChildren } from "preact"
 import classnames from 'classnames'
@@ -10,6 +10,7 @@ type MediaTextProps = {
 	style?: string,
 	image?: string,
 	imagePosition?: 'left'|'right',
+	breakpoint?: 'small'|'medium'|'large'|'x-large',
 	align?: 'start'|'center'|'end',
 	children?: ComponentChildren
 }
@@ -22,12 +23,14 @@ export default function MediaText(props: MediaTextProps): JSX.Element {
 		image,
 		imagePosition = 'left',
 		align = 'center',
+		breakpoint = 'medium',
+		//media width, content width
 		...attributes
 	} = props
 
 	const classes = classnames(
 		'ui-mediatext grid grid--gutter',
-		imagePosition && `ui-mediatext--${imagePosition}`,
+		imagePosition === 'right' && `row-reverse@${breakpoint}`,
 		align && `flex-${align}`,
 		className,
 	)
@@ -36,11 +39,11 @@ export default function MediaText(props: MediaTextProps): JSX.Element {
 
 	return <>
 			<div class={classes}>
-				<div class="ui-mediatext__media">
+				<div class={`ui-mediatext__media col-6@${breakpoint}`}>
 					<img src={image} class="object-fit" />
 				</div>
-				<div class="ui-mediatext__content">
-					<div class="ui-mediatext__content__inner">
+				<div class={`ui-mediatext__content col-6@${breakpoint}`}>
+					<div class="ui-mediatext__content__inner first-last">
 						{children}
 					</div>
 				</div>

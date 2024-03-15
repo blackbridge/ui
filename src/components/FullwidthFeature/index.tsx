8d7409eeb37d969ed64a3e549d0ebd873a3a3ab3
Jake Adams <jake.adams@blackbridge.co.uk>
2024-03-15 19:25:49 +0000

    Fullwidth Feature: If no children, only show an image

--- src/components/FullwidthFeature/index.tsx
+++ src/components/FullwidthFeature/index.tsx
@@ -31,9 +31,9 @@ export default function FullwidthFeature(props: FullwidthFeatureProps): JSX.Elem
 			<div class={classes} {...attributes}>
 				<img class="ui-fullwidth-feature__media object-fit--absolute object-fit--cover" src={image} />
 				{overlay && <div class="ui-fullwidth-feature__overlay"></div>}
-				<div class="ui-fullwidth-feature__content">
+				{children && <div class="ui-fullwidth-feature__content">
 					{children}
-				</div>
+				</div>}
 			</div>
 		</>
 		
